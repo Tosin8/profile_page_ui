b5f41ccf68474016e692286e239915ac230f0ef2
Tosin8 <tosinezekiel8@gmail.com>
2022-07-05 11:23:51 +0100

    Adding the stat_widget file

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,137 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+      title: 'Profile_Page',
+      debugShowCheckedModeBanner: false,
+      home: Profile_Ui()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+// ignore: camel_case_types
+class Profile_Ui extends StatefulWidget {
+  const Profile_Ui({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<Profile_Ui> createState() => _Profile_UiState();
+}
+
+// ignore: camel_case_types
+class _Profile_UiState extends State<Profile_Ui> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        leading: const Drawer(),
+        title: const Text(
+          'Profile Page',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        elevation: 0.0,
+        backgroundColor: Colors.white,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      body: Center(
+        child: ListView(
+          children: [
+            const CircleAvatar(
+              radius: 56,
+              backgroundImage: AssetImage('assets/images/model.jpg'),
+            ),
+            const SizedBox(height: 12.0),
+            const Center(
+              child: Text(
+                'Elton Marcs',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            const Center(
+              child: Text(
+                '@elton_marcs8',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            const Center(
+              child: Text(
+                'Brand Influencer',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 12.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                    child: Column(children: const [
+                  Text('Posts',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                      )),
+                  Text(
+                    '30',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  )
+                ])),
+                Expanded(
+                    child: Column(children: const [
+                  Text('Followers',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                      )),
+                  Text(
+                    '2.5k',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  )
+                ])),
+                Expanded(
+                    child: Column(children: const [
+                  Text('Following',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                      )),
+                  Text(
+                    '76',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  )
+                ])),
+              ],
+            ),
+            const SizedBox(height: 18),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ElevatedButton(onPressed: () {}, child: const Text('Follow')),
+                const SizedBox(width: 8.0),
+                OutlinedButton(onPressed: () {}, child: const Text('Message'))
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
